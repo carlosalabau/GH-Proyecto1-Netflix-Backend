1969af1c3dc6aabf280feffdd0d7b60395955e42
G1ovann16 <glandaburo92@gmail.com>
2020-04-13 13:42:10 +0200

    ready header

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './containers/home/home.component';
+import { MovieComponent } from './containers/movie/movie.component';
+import { SingInComponent } from './containers/sing-in/sing-in.component';
+import { SingUpComponent } from './containers/sing-up/sing-up.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'movie', component: MovieComponent },
+  {path: 'singin', component: SingInComponent },
+  {path: 'sinup', component: SingUpComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
