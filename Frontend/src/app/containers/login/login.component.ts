cd8564a73ec23a2787c269e7b7b6fd946cc9913e
G1ovann16 <glandaburo92@gmail.com>
2020-04-14 15:28:01 +0200

    services

--- Frontend/src/app/containers/login/login.component.ts
+++ Frontend/src/app/containers/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from '../../services/users.service';
+import { NgForm } from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  users: {
+  };
+  constructor(
+    public userService: UsersService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
+  addUserLogin(userform: NgForm){
+    this.users = userform.value;
+    this.userService.setNewLogin(this.users)
+    .subscribe(msn => console.log(this.users));
+    this.router.navigate(['/movie']);
+        }
 
 }
--- Frontend/src/app/services/users.service.ts
+++ Frontend/src/app/services/users.service.ts
@@ -13,13 +13,22 @@ export class UsersService {
   }
 
     // POST
-  setTask(body: any, id: number){
-    return this.http.post<any>(`http://localhost:3000/usuarios/actualizar/${id}`, body);
-  }
-  // DELETE
-    erase(id: number){
-      return this.http.delete(`http://localhost:3000/ususarios/eliminar/${id}`);
+    setNewRegister(body: any){
+      return this.http.post<any>('http://localhost:3000/usuarios/registro', body);
+    }
+    setNewLogin(body: any){
+      return this.http.post<any>('http://localhost:3000/usuarios/login', body);
+    }
+    setNewLogout(body: any){
+      return this.http.post<any>('http://localhost:3000/usuarios/logout', body);
     }
 
-
+    // PUT
+  updateUser(body: any, id: number){
+    return this.http.put<any>(`http://localhost:3000/usuarios/actualizar/${id}`, body);
+  }
+  // DELETE
+    deleteUser(id: number){
+      return this.http.delete(`http://localhost:3000/ususarios/eliminar/${id}`);
+    }
 }
