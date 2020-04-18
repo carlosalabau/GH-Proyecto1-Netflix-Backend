6f3c3144a7f961bedab5abd6840245f4a33bfa07
G1ovann16 <glandaburo92@gmail.com>
2020-04-18 16:10:09 +0200

    upimage

--- Frontend/src/app/containers/admin/admin.component.ts
+++ Frontend/src/app/containers/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from '../../services/users.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  // uploadedFiles: Array <File>;
+  //  formData;
+  constructor(private adminServices: UsersService) { }
 
   ngOnInit(): void {
   }
 
+// upload(){
+// let  formData = new formData();
+// // tslint:disable-next-line: prefer-for-of
+// for (let i = 0; i < this.uploadedFiles.length; i++) {
+// formData.append('uploads[]', this.uploadedFiles[i], this.uploadedFiles[i].name);
+// }
+// this.adminServices.uploadFile(formData).subscribe((res) => {
+// console.log(res);
+// });
+
+// }
+// filechange(e){
+// this.uploadedFiles = e.target.files;
+// }
+
 }
+
+
+// https://www.youtube.com/watch?v=-BVrRGrBhBA
